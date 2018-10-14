58512f88645da1dbaa57eab73b1cb90c48fe37e9
Apple Hurtado <aph@Apples-MacBook-Pro.local>
2018-10-14 16:41:28 +0800

    add mobile PH phone number validation, add model validation in User model

--- app/Domains/Users/Models/User.php
+++ app/Domains/Users/Models/User.php
@@ -2,9 +2,19 @@
 
 namespace Acme\Domains\Users\Models;
 
+use FiveSay\Laravel\Model\ExtTrait;
 use Illuminate\Database\Eloquent\Model;
+use libphonenumber\PhoneNumberType;
+use Illuminate\Validation\Rule;
 
 class User extends Model
 {
-    //
+	use ExtTrait;
+
+    public $rules = [
+        'mobile' => [
+            'required' 	   => 'mobile field is required',
+            'phone:PH'	   => 'mobile field must be a valid PH phone number',
+        ],
+    ];
 }
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -13,11 +13,39 @@ class UserTest extends TestCase
 	use RefreshDatabase;
 
     /** @test */
-    public function user_has_mobile_attribute()
+    public function user_has_required_mobile_attribute()
+    {
+    	$this->expectException(\Illuminate\Validation\ValidationException::class);
+
+    	$mobile = null;
+    	factory(User::class)->create(compact('mobile'));
+    }
+
+    /** @test */
+    public function user_has_unique_mobile_attribute()
+    {
+    	$this->expectException(\Exception::class);
+
+    	$mobile = config('acme.test.user.mobile');
+    	factory(User::class)->create(compact('mobile'));
+    	factory(User::class)->create(compact('mobile'));
+    }
+
+    /** @test */
+    public function user_has_persistent_mobile_attribute()
     {
     	$mobile = config('acme.test.user.mobile');
-        factory(User::class)->create(compact('mobile'));
+    	factory(User::class)->create(compact('mobile'));
 
         $this->assertDatabaseHas('users', compact('mobile'));
+    }
+
+    /** @test */
+    public function user_mobile_field_has_ph_phone_validation()
+    {
+    	$this->expectException(\Illuminate\Validation\ValidationException::class);    	
+    	$mobile = '1234567';
+
+    	factory(User::class)->create(compact('mobile'));
     }
 }
